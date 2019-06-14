8326937cb60d530bb471396ad5e85f601868fb12
Juyoung Park <james.park.victoria@gmail.com>
2019-06-13 22:30:59 -0700

    Stylize header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Header.scss";
+
 class Header extends React.Component {
   render() {
     return (
-      <div className="basetext1 medium">
+      <div className="Header">
         <Link to="/">HOME</Link>
-        <Link to="/code">code</Link>
+        <div>
+          <Link to="/code">code</Link>
+        </div>
         <Link to="/design">design</Link>
         <Link to="/">about me</Link>
       </div>
