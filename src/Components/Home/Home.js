a6a99bcd1b9cf2da40e44d020a7c66b96c666561
Juyoung Park <james.park.victoria@gmail.com>
2019-06-16 17:06:32 -0700

    Portrait image scaling and positioning

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,56 +9,60 @@ import Blocker from "../../assets/blocker.png";
 class Home extends React.Component {
   render() {
     return (
-      <div className="basetext1 medium">
-        <div className="title">about me</div>
-        <div className="Portrait">
-          <img src={Blocker} className="Blocker" />
-          <img src={Portrait} className="Portrait" />
-          <img src={Background} className="Background" />
-        </div>
-        <div className="subheader1">James Juyoung Park</div>
-        <div>
-          Software Engineering Student at the{" "}
-          <strong>University of Victoria</strong>
-        </div>
-        <div>
-          Co-op Software Developer at <strong>PDFTron Systems</strong>
-        </div>
-        <div>
-          <a
-            href="https://drive.google.com/file/d/1z5UOpLnqH-6NkjqhB9vwLI3ty1EcMGl2/view?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Resume
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://github.com/jamesjypark"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://dribbble.com/jamespark"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dribbble
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://www.linkedin.com/jamesjypark"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </a>
+      <div className="Home">
+        <div className="basetext1 medium">
+          <div className="title">about me</div>
+          <div className="Portrait">
+            <img src={Blocker} className="Blocker" />
+            <img src={Portrait} className="Portrait" />
+            <img src={Background} className="Background" />
+          </div>
+          <div className="subheader1">James Juyoung Park</div>
+          <div className="description">
+            Software Engineering Student at the{" "}
+            <strong>University of Victoria</strong>
+          </div>
+          <div className="description">
+            Co-op Software Developer at <strong>PDFTron Systems</strong>
+          </div>
+          <div className="links">
+            <div>
+              <a
+                href="https://drive.google.com/file/d/1z5UOpLnqH-6NkjqhB9vwLI3ty1EcMGl2/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Resume
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://github.com/jamesjypark"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://dribbble.com/jamespark"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Dribbble
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://www.linkedin.com/jamesjypark"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     );
