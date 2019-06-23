777c7eafb2d8165ab84e886f7cf575631faf51ce
Juyoung Park <james.park.victoria@gmail.com>
2019-06-23 11:21:02 -0700

    Implemented styling for projectCard

--- src/Components/Code/Code.js
+++ src/Components/Code/Code.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { NavLink, Switch, Route } from "react-router-dom";
 import Meditap from "../Projects/Meditap/Meditap";
-import MeditapJSON from "../Projects/Meditap/Meditap.json";
 import Askey from "../Projects/Askey/Askey";
 import RealTime from "../Projects/RealTime/RealTime";
 import ProjectCard from "../ProjectCard/ProjectCard";
 
+import MeditapJSON from "../Projects/Meditap/Meditap.json";
+import AskeyJSON from "../Projects/Askey/Askey.json";
+import RealTimeJSON from "../Projects/RealTime/RealTime.json";
+
+import MeditapLogo from "../../assets/meditap.svg";
+import AskeyLogo from "../../assets/askey.svg";
+import RealTimeLogo from "../../assets/realtime.svg";
+
+import "./Code.scss";
+
 function CodeList() {
   return (
-    <div>
+    <div className="CodeList">
       <div className="title">code</div>
-
       <NavLink className="link" to="/code/meditap">
-        <ProjectCard {...MeditapJSON} />
+        <ProjectCard image={MeditapLogo} {...MeditapJSON} />
       </NavLink>
-
-      {/* <NavLink to="/code/askey">
-        <ProjectCard />
+      <NavLink className="link" to="/code/askey">
+        <ProjectCard image={AskeyLogo} {...AskeyJSON} />
       </NavLink>
-
-      <NavLink to="/code/real-time-collaboration">
-        <ProjectCard />
-      </NavLink> */}
+      <NavLink className="link" to="/code/real-time-collaboration">
+        <ProjectCard image={RealTimeLogo} {...RealTimeJSON} />
+      </NavLink>
     </div>
   );
 }
--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -4,17 +4,24 @@ import "./ProjectCard.scss";
 
 class ProjectCard extends React.Component {
   render() {
-    const { title, duration, technology, summary } = this.props;
+    const { title, duration, technology, summary, image } = this.props;
     return (
       <div className="ProjectCard">
+        <div className="image-container">
+          <img src={image} />
+        </div>
         <div className="text">
           <div className="subheader2">{title}</div>
           <div className="basetext2 book">{duration}</div>
           <div className="basetext2 book">{technology}</div>
-          {summary.forEach(summary => {
-            return <div className="basetext2 book">{summary}</div>;
-          })}
-          <div className="basetext2 book">{summary}</div>
+          <div className="summary">
+            {summary.map(text => (
+              <div className="basetext2 book">{text}</div>
+            ))}
+            {summary.forEach(summary => {
+              return <div className="basetext2 book">{summary}</div>;
+            })}
+          </div>
         </div>
       </div>
     );
