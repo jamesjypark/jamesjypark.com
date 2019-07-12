f71fe67e35fb88a9500c8fc2ece0e65bf40b0464
Juyoung Park <james.park.victoria@gmail.com>
2019-07-11 20:52:14 -0700

    Hover style update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Header.scss";
 import logo from "../../assets/logo.png";
 
@@ -7,19 +7,19 @@ class Header extends React.Component {
   render() {
     return (
       <div className="Header">
-        <Link to="/" activeClassName="link">
+        <NavLink to="/" activeClassName="link">
           <img src={logo} />
-        </Link>
+        </NavLink>
         <div className="links">
-          <Link to="/code" className="link" activeClassName="active">
+          <NavLink to="/code" className="link" activeClassName="active">
             code
-          </Link>
-          <Link to="/design" className="link" activeClassName="active">
+          </NavLink>
+          <NavLink to="/design" className="link" activeClassName="active">
             design
-          </Link>
-          <Link exact to="/" className="link" activeClassName="active">
+          </NavLink>
+          <NavLink exact to="/" className="link" activeClassName="active">
             about me
-          </Link>
+          </NavLink>
         </div>
       </div>
     );
